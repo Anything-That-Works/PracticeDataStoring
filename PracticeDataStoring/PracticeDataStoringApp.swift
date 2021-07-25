4263e0d6485d8397f0453835dc251b17eb1f7792
Promal B <promal.barua.cse@gmail.com>
2021-07-26 02:47:48 +0600

    Initial Commit

---
+++ PracticeDataStoring/PracticeDataStoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeDataStoringApp.swift
+//  PracticeDataStoring
+//
+//  Created by Promal on 26/7/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticeDataStoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
